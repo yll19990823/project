2fc34e3e6c03fb3a7fd3248de659d04d0475da7d
追光者 <57619048+yll19990823@users.noreply.github.com>
2021-04-23 11:36:54 +0800

    Delete 专家信息管理系统 directory

--- 专家信息管理系统/user.sql
+++
@@ -1,7 +0,0 @@
-create table majoruser(
-username varchar(50) Not Null,
-usernumber varchar(50) PRIMARY KEY,
-password varchar(50) Not Null, 
-phone varchar(50) Not Null, 
-ustime DateTime DEFAULT GetDate(), 
-);
